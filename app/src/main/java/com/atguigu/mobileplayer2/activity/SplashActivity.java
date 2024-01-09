132f3e5abb75251f64343f1546bc7b2e2c9f92c6
gujunqi <gujunqi2018@gmail.com>
2024-01-09 11:34:14 +0800

    第一步 启动页

---
+++ app/src/main/java/com/atguigu/mobileplayer2/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.atguigu.mobileplayer2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.atguigu.mobileplayer2.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
